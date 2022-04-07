c42cc48c2fee0a7827f9dcd65cddf7769b1611ab
Sebastian09-09 <73454646+Sebastian09-09@users.noreply.github.com>
2022-04-07 15:14:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ PATH = "C:\\Users\\ffuru\\Desktop\\pokemon-go-notifier\\chromedriver.exe"
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
 options = webdriver.ChromeOptions()
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.headless = True
 options.add_argument(f'user-agent={user_agent}')
 options.add_argument("--window-size=1920,1080")
@@ -29,8 +30,7 @@ options.add_argument("--start-maximized")
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
-#driver = webdriver.Chrome(PATH)
+driver=webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 driver.get(f'https://web.whatsapp.com/send?phone={number}')
 
 
